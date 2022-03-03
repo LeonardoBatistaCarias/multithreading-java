9b814c082f8d27204c7094f016409571a40ef4ee
Leonardo Batista Carias <leonardo1carias@gmail.com>
2022-03-03 19:43:23 -0300

    Calls in product service using threads

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 # parallel-asynchronous
-=======
-# parallel-asyncronous
->>>>>>> a34e36e8ba5845ef21faa29c8e12b293185f0281
 Repository for the course "Multithreading, Parallel & Asynchronous Coding in Modern Java", which aims to learn Java Multithreading using the ParallelalStreams API and CompletableFuture API
 
 ## Swagger-UI
---
+++ src/main/java/com/leonardobatistacarias/thread/ProductServiceUsingThread.java
@@ -0,0 +1,90 @@
+package com.leonardobatistacarias.thread;
+
+import com.leonardobatistacarias.domain.Product;
+import com.leonardobatistacarias.domain.ProductInfo;
+import com.leonardobatistacarias.domain.Review;
+import com.leonardobatistacarias.service.ProductInfoService;
+import com.leonardobatistacarias.service.ReviewService;
+
+import static com.leonardobatistacarias.util.CommonUtil.stopWatch;
+import static com.leonardobatistacarias.util.LoggerUtil.log;
+
+public class ProductServiceUsingThread {
+    private ProductInfoService productInfoService;
+    private ReviewService reviewService;
+
+    public ProductServiceUsingThread(ProductInfoService productInfoService, ReviewService reviewService) {
+        this.productInfoService = productInfoService;
+        this.reviewService = reviewService;
+    }
+
+    public Product retrieveProductDetails(String productId) throws InterruptedException {
+        stopWatch.start();
+
+        ProductInfoRunnable productInfoRunnable = new ProductInfoRunnable(productId);
+        Thread productInfoThread = new Thread(productInfoRunnable);
+
+        ReviewRunnable reviewRunnable = new ReviewRunnable(productId);
+        Thread reviewThread = new Thread(reviewRunnable);
+
+        productInfoThread.start();
+        reviewThread.start();
+
+        productInfoThread.join();
+        reviewThread.join();
+
+        ProductInfo productInfo = productInfoRunnable.getProductInfo();
+        Review review = reviewRunnable.getReview();
+
+        stopWatch.stop();
+        log("Total Time Taken : "+ stopWatch.getTime());
+        return new Product(productId, productInfo, review);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ProductInfoService productInfoService = new ProductInfoService();
+        ReviewService reviewService = new ReviewService();
+        ProductServiceUsingThread productService = new ProductServiceUsingThread(productInfoService, reviewService);
+        String productId = "ABC123";
+        Product product = productService.retrieveProductDetails(productId);
+        log("Product is " + product);
+
+    }
+
+    private class ProductInfoRunnable implements Runnable {
+        private ProductInfo productInfo;
+        private String productId;
+
+        public ProductInfoRunnable(String productId) {
+            this.productId = productId;
+        }
+
+        public ProductInfo getProductInfo() {
+            return productInfo;
+        }
+
+        @Override
+        public void run() {
+            productInfo = productInfoService.retrieveProductInfo(productId);
+        }
+    }
+
+    private class ReviewRunnable implements Runnable {
+        private String productId;
+        private Review review;
+
+        public ReviewRunnable(String productId) {
+            this.productId = productId;
+        }
+
+        public Review getReview() {
+            return review;
+        }
+
+        @Override
+        public void run() {
+            review = reviewService.retrieveReviews(productId);
+        }
+    }
+}
